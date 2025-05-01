fbf716f9de6f658894469a47a9a617eb09af5f39
Pushan <pushansinha8@gmail.com>
2025-05-01 12:18:26 +0530

    Post Deployment Changes

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://tranquil-boba-425d35.netlify.app/", 
   methods: "GET,POST,PUT,DELETE",
   credentials: true
 }));
